51af1f1499e7ab0128a4ec5a4305354cde69b4e8
mattcoosh <mcuschie@risd.edu>
2021-10-14 14:12:20 -0400

    Update click.js

--- click.js
+++ click.js
@@ -1,4 +1,9 @@
-document.getElementById('calenderlink').onclick = function executeOnClick() {
-    return true;
-    // Do stuff.
-};
+var element_to_scroll_to = document.getElementById('anchorName2');
+// Or:
+var element_to_scroll_to = document.querySelectorAll('.my-element-class')[0];
+// Or:
+var element_to_scroll_to = $('.my-element-class')[0];
+// Basically `element_to_scroll_to` just have to be a reference
+// to any DOM element present on the page
+// Then:
+element_to_scroll_to.scrollIntoView();
